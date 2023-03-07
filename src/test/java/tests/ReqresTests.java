72309273252220db45c7731e31a9352a195d9ff0
Konstantin Zolotovskiy <konstantin.zolotovskiy@brandmaker.com>
2023-03-07 23:50:48 +0700

    init commit

---
+++ src/test/java/tests/ReqresTests.java
@@ -0,0 +1,79 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ReqresTests {
+
+    @Test
+    void createUserTest() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void updateUserTest() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void deleteUserTest() {
+
+        given()
+                .contentType(JSON)
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    void negativeLoginTest() {
+        String data = "{ \"email\": \"peter@klaven\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void negativeRegistrationTest() {
+        String data = "{ \"password\": \"superPassword\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+}
